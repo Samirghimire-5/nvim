225ed6ec6b3e701f0eb46a3e1e43548def893d74
Samirghimire-5 <samirghimire9841@gmail.com>
2026-01-23 21:07:22 +0545

    added Liveserver start & end keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,18 @@ map("i", "jk", "<Esc>", { silent = true })
 map("v", "jk", "<Esc>", { silent = true })
 map("t", "jk", [[<C-\><C-n>]], { silent = true })
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- npm install -g live-server  (install live-server globally in your machine)
+-- Live Server
+vim.keymap.set("n", "<leader>ls", function()
+	vim.fn.jobstart({ "live-server", "." }, {
+		detach = true,
+	})
+end, { desc = "Start Live Server" })
+
+-- Stop Live Server
+vim.keymap.set("n", "<leader>lq", function()
+	vim.fn.jobstart({ "pkill", "live-server" }, {
+		detach = true,
+	})
+end, { desc = "Stop Live Server" })
